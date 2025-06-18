2f246401bd86e9e088eab530860bb7e9e6b0d022
lesuho <75497467@continental.edu.pe>
2025-06-18 14:45:06 -0500

    Delete FRONTEND directory

--- FRONTEND/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from './axios'
-
-export const registerRequest = (user) => axios.post(`/auth/register`, user)
-
-export const loginRequest = (user) => axios.post(`/auth/login`, user)
-
-export const verifyTokenRequest = () => axios.get(`/auth/verify`)
