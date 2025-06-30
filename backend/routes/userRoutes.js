d4b3e9013fbdcc37b2fd47586c58a10e7bdb4ef2
lesuho <75497467@continental.edu.pe>
2025-06-30 14:26:00 -0500

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkUserExists } = require('../controllers/userController');
-const { protect } = require('../middleware/auth');
-
-// Ruta para verificar si un usuario existe
-router.get('/check/:email', protect, checkUserExists);
-
-module.exports = router;
